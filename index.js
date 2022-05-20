00a517dbb827cd8fb26ab80fdd2db49e77887fe9
KramesDt <lordkratos365@gmail.com>
2022-05-20 23:43:19 +0100

    commit

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import http from 'http';
+const express = require("express");
+const http = require("http");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
-
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an express server</h1></body></html>');
-
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an express server</h1></body></html>");
 });
 
+// const server = http.createServer(app);
 
+app.listen(port, hostname, () => {
+  console.log(`Server reunning at http://${hostname}:${port}`);
+});
