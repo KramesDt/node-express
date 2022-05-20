2e6384185268ee19e0f7df315541374198efec53
KramesDt <lordkratos365@gmail.com>
2022-05-20 23:22:56 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import http from 'http';
+
+const hostname = 'localhost';
+const port = 3000;
+
+
+const app = express();
+
+app.use((req, res, next) =>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an express server</h1></body></html>');
+
+});
+
+
