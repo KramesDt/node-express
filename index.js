a11b238e83293db9074c79f3dc80f1d0cd3feccb
KramesDt <lordkratos365@gmail.com>
2022-05-21 02:35:13 +0100

    Implemented middleware

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
-
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+
 
 app.use((req, res, next) => {
   res.statusCode = 200;
